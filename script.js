b2bdc297a2f36ea8918823a8e66bb45610acedab
Sami Ahmed Mirza <samiahmed.code@gmail.com>
2025-10-14 09:07:43 +0000

    Test Commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello")
+/*Q1: Write a function deepFilterFalsy(arr) that takes a nested array of any depth and returns a
+flattened array with all falsy values removed. */
+
+function deepfilterfalsy(){
+
+let arr=[0, 1, "",false, "JS"];
+let a = 99
+
+    for(let i = 0; i < arr.length ; i++){
+        if(arr[i] == false){
+            arr[i] = a; 
+       }
+       
+    }
+    console.log(arr);
+}
+deepfilterfalsy()
