5673d55f549cd64f817fd50e2f2f43edac3cb94d
Sami Ahmed Mirza <samiahmed.code@gmail.com>
2025-10-24 13:04:24 +0000

    half

--- string3.js
+++ string3.js
@@ -28,6 +28,81 @@ uppercase letter.
 
 Testcases: "hello_world" → "helloWorld" */
 
-function Case(str){
-    let 
+// function Case(str){
+//     let newStr = str.split("_");
+//     let res = ""
+//     let result = ""
+//     console.log(newStr)
+
+//     for (let i = 1; i < newStr.length; i++) {
+//         newStr[i] = newStr[i][0].toUpperCase() + newStr[i].slice(1)
+//         res += newStr[i]
+//         result = newStr[0] + res
+
+//     }
+//     console.log(result);
+// }
+
+
+// Case("hello_world");
+
+
+
+
+/*Q3: You are given a string that may contain both text and numbers. Extract all the numbers from the string and return them as an array of integers.
+If there are no numbers, return an empty array.
+
+Testcases: "I have 2 apples and 5 bananas" → [2, 5] */
+
+// function Return(str){
+//     let newStr = str;
+//     let res = "0123456789"
+//     let result = []
+//     console.log(newStr)
+
+//     for (let i = 0; i < newStr.length; i++) {
+//         if(res.includes(newStr[i])){
+//             result.push(newStr[i])
+//         }
+//     }
+//     console.log(result)
+// }
+
+// Return("I have 2 apples and 5 bananas")
+
+function extract(str) {
+  let matches = str.match(/\d+/g);
+ 
+  if (!matches) return [];
+
+  return matches.map(Number);
+}
+
+console.log(extract("I have 2 apples and 5 bananas"))
+
+
+
+
+
+/*Q4: A string may contain HTML tags like <p>, <div>, <b>, etc. Remove all the tags and return only the plain text content without any markup.
+
+Testcases: "<p>Hello</p>" → "Hello" */
+
+// function remove(str){
+//     let 
+// }
+
+// remove("<p>Hello</p>")
+
+
+
+
+
+/*Q5: Find the character that occurs most frequently in the given string (ignore case). If more than one character has the same highest frequency, return the alphabetically first
+one.
+
+Testcases: "mississippi" → "i" */
+
+function same(str){
+
 }
