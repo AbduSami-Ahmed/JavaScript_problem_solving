3a51fbe786ae4ef0d8539dcf8d3647a31d574b3b
Sami Ahmed Mirza <samiahmed.code@gmail.com>
2025-10-24 10:46:52 +0000

    almost

---
+++ string3.js
@@ -0,0 +1,33 @@
+/*Q1: You are given a string. Find and return the first character that does not repeat anywhere
+else in the string. The comparison should be case-insensitive, but you should return the character in its
+original case. If every character repeats, return null.
+
+Testcases: "stress" → "t" , "aabbcc" → null */
+
+// function repeat(str){
+//     let newStr = str.toLowerCase();
+
+//     for (let i = 0; i < newStr.length; i++) {
+//         let char = newStr[i]
+
+//         if (newStr.indexOf(char) == newStr.lastIndexOf(char)) {
+//             return str[i];
+//         }
+//     }
+//     return null
+// }
+
+// console.log(repeat ("stress"));
+
+
+
+
+/*Q2: Given a string written in snake_case (words separated by underscores), convert it into
+camelCase format. The first word should remain lowercase, and every subsequent word should start with an
+uppercase letter.
+
+Testcases: "hello_world" → "helloWorld" */
+
+function Case(str){
+    let 
+}
